e2d36d6ca9452d8cdf01c2901f7945e53b7bee2f
xinwen <hxinwen1218@sina.com>
2024-01-05 18:25:47 +0800

    test change json null into python None

--- test/test_print_json.py
+++ test/test_print_json.py
@@ -3,20 +3,26 @@ from torch.utils.data import DataLoader
 from utils.dataset import CustomDataset
 from utils.util import getSaveFileName, getDevice
 from models.train import testModel
-from utils.visualize import vis_table
+from utils.visualize import vis_table, get_params
 
-# 参数
-file_name = './res_trigonometric/fcn/FCN_2024-01-03-20-16.pth'
-device = getDevice()
-print("The device U can Use is: ", device)
+def testModel():
+    # 参数
+    file_name = './res_trigonometric/fcn/FCN_2024-01-03-20-16.pth'
+    device = getDevice()
+    print("The device U can Use is: ", device)
 
-# 数据
-test_dataset = CustomDataset(20, "trigonometric", "random", "sin", None, 10, None, 10, 42)
-test_loader = DataLoader(test_dataset, batch_size = 1, shuffle = False)
+    # 数据
+    test_dataset = CustomDataset(20, "trigonometric", "random", "sin", None, 10, None, 10, 42)
+    test_loader = DataLoader(test_dataset, batch_size = 1, shuffle = False)
 
-# 测试
-# testModel(file_name = None, test_loader = None, model = None, device = None, criter = 'MSE', model_type = None)
-pred_y, test_loss = testModel(file_name, test_loader, model =  None, device = device , criter = 'MSE', model_type = 'FCN')
+    # 测试
+    # testModel(file_name = None, test_loader = None, model = None, device = None, criter = 'MSE', model_type = None)
+    pred_y, test_loss = testModel(file_name, test_loader, model =  None, device = device , criter = 'MSE', model_type = 'FCN')
 
-vis_table(test_dataset, pred_y, test_loss)
-# save_json(file_name, test_dataset, pred_y, test_loss)
+    vis_table(test_dataset, pred_y, test_loss)
+
+if __name__ == "__main__":
+    dtparams, traparams, mdlparams, tstparams = get_params('./configs/params_fcn.json')
+
+    print("all data parameters: ", dtparams)
+    print("test of null in json: ", dtparams["test_key"])
--- test_fcn.py
+++ test_fcn.py
@@ -9,37 +9,30 @@ from utils.util import getSaveFileName, getDevice
 from models.networks import FCNModel
 from models.train import testModel, Trainer
 
-# TODO: [x]网络测试 【完成，结论：可用】
+# TODO: 网络测试 【完成，结论：可用】
 # TODO: 检查 GPU 是否可用【完成，结论：可用】
 # TODO: 检查数据集是否正确
 device = getDevice()
 print("The device U can Use is: ", device)
 
-# return params['data'], params['training'], params['model'], params['testing']
 dtparams, traparams, mdlparams, tstparams = get_params('./configs/params_fcn.json')
 
 # ****************************** DATA ****************************************
-# init(num_points = 100, function_type='polynomial', data_type = 'random', tri_function = 'sin', degree=2, scale = 10, a=1, k=1, seed=None)
 train_dataset = CustomDataset(dtparams['num_points'], dtparams['function_type'], dtparams['data_type'], dtparams['tri_function'], None, dtparams['data_scale'], None, dtparams['tri_k'], None)
 # print("Polynomial Data:")
 # for x, y in train_dataset:
 #     print(f"{x.item():.6f}: {y.item():.6f}")
 train_loader = DataLoader(train_dataset, batch_size = dtparams['batch_size'], shuffle = True)
 
-# init(num_points = 100, function_type='polynomial', data_type = 'random', tri_function = 'sin', degree=2, scale = 10, a=1, k=1, seed=None)
 test_dataset = CustomDataset(tstparams['num_points'], dtparams['function_type'], tstparams['data_type'], dtparams['tri_function'], None, tstparams['data_scale'], None, dtparams['tri_k'], tstparams['seed'])
 test_loader = DataLoader(test_dataset, tstparams['batch_size'], shuffle = False)
 
 # ****************************** MODEL ****************************************
-# init(input_dim, hidden_dims, output_dim, activate)
 model = FCNModel(mdlparams['input_dim'], mdlparams['hidden_dims'], mdlparams['output_dim'], mdlparams['activation'])
 
 # ****************************** TRAIN ****************************************
-# getSaveFileName(model_name = 'FCN', path = './res/')
 file_name = getSaveFileName(mdlparams['model_type'], traparams['res_dir'])
-# init(model, train_loader, lr=0.01, criter = 'MSE', checkpoint_dir = './cache/', model_type = None)
 trainer = Trainer(model, train_loader, test_loader, traparams['learning_rate'], traparams['criter'], traparams['checkpoint'], mdlparams['model_type'])
-# train(num_epochs = 5000, save_interval = 1000, file_name = None, device = None)
 trainer.train(traparams['num_epochs'], traparams['save_interval'], file_name, device )
 
 # ****************************** VISUALIZATION ****************************************
